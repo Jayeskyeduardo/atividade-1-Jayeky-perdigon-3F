dfdfee9d9ae3566523a5d0736d0f17f40853d89e
JayeskyEduardo <164092285+Jayeskyeduardo@users.noreply.github.com>
2024-08-05 22:57:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const meunome = "jayesky";
+const idade = 18;
+const altura = 1.73;
+const massa = 55;
+const cidade = "São José dos Pinhas";
+
+console.log(meunome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidade);
